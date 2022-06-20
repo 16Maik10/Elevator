d3c729e71bfef8a9fdd0a00fa8f8d1bceda15f7e
MB13WIN10 <mihailbab13@yandex.ru>
2022-06-20 17:07:37 +0300

    normal lvl is done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { onBeforeMount, onMounted, reactive, ref, watch } from "vue";
 const position = ref(0.5);
 const currentFloor = ref(1);
 const directionFloor = ref(0);
@@ -24,20 +24,22 @@ const moving = () => {
     currentFloor.value--;
   }
 };
-const setPosition = (floor, currentTiming) => {
+const setPosition = (floor, currentTiming, wasMoving = false) => {
   lifting.value = true;
   directionFloor.value = floor;
-  moving();
+  if (!wasMoving) {
+    moving();
+  }
   const IID = setInterval(moving, 1001);
   setTimeout(() => {
     clearInterval(IID);
     btnsState[floor] = false;
     currentStopedFloor.value = currentFloor.value;
     setTimeout(() => {
-      lifting.value = false;
       elevatorIsRest.value = false;
       moveOrQue(queue.value.shift());
     }, 3000);
+    lifting.value = false;
     elevatorIsRest.value = true;
   }, currentTiming);
 };
@@ -47,17 +49,66 @@ const moveOrQue = (floor) => {
   }
   btnsState[floor] = true;
   currentStopedFloor.value = 0;
-  if (lifting.value) {
+  if (lifting.value || elevatorIsRest.value) {
     !queue.value.includes(floor) && queue.value.push(floor);
   } else {
     liftDirection.value = floor > currentFloor.value;
-    setPosition(
-      floor,
-      Math.abs(floor - currentFloor.value) * 1000,
-      liftDirection.value
-    );
+    setPosition(floor, Math.abs(floor - currentFloor.value) * 1000);
   }
 };
+watch(btnsState, (newBtnsState) => {
+  sessionStorage.setItem("btnsState", JSON.stringify(newBtnsState));
+});
+watch(queue.value, (newQueue) => {
+  sessionStorage.setItem("queue", JSON.stringify(newQueue));
+});
+watch(directionFloor, (newDirectionFloor) => {
+  sessionStorage.setItem("directionFloor", newDirectionFloor);
+});
+watch(currentFloor, (newCurrentFloor) => {
+  sessionStorage.setItem("currentFloor", newCurrentFloor);
+});
+watch(lifting, (newLifting) => {
+  sessionStorage.setItem("lifting", newLifting);
+});
+onBeforeMount(() => {
+  const _btnsState = JSON.parse(sessionStorage.getItem("btnsState"));
+  if (_btnsState) {
+    for (let key in _btnsState) {
+      btnsState[key] = _btnsState[key];
+    }
+  }
+  const _queue = JSON.parse(sessionStorage.getItem("queue"));
+  if (_queue) {
+    for (let value of _queue) {
+      queue.value.push(value);
+    }
+  }
+  const _currentFloor = +sessionStorage.getItem("currentFloor");
+  if (_currentFloor) {
+    currentFloor.value = _currentFloor;
+    position.value = 0.5 + (currentFloor.value - 1) * 20;
+  }
+});
+onMounted(() => {
+  const _directionFloor = +sessionStorage.getItem("directionFloor");
+  if (_directionFloor) {
+    liftDirection.value = _directionFloor > currentFloor.value;
+    const _lifting = sessionStorage.getItem("lifting");
+    if (_lifting === "true") {
+      setPosition(
+        _directionFloor,
+        Math.abs(_directionFloor - currentFloor.value) * 1000
+      );
+    } else {
+      setPosition(
+        _directionFloor,
+        Math.abs(_directionFloor - currentFloor.value) * 1000,
+        true
+      );
+    }
+  }
+});
 </script>
 
 <template>
@@ -94,7 +145,7 @@ const moveOrQue = (floor) => {
         bottom: `${position}vh`,
       }"
     >
-      <p :class="{ showDirection: lifting }">
+      <p :class="{ showDirection: lifting || elevatorIsRest }">
         <span v-if="liftDirection">&uarr;</span>
         <span v-else>&darr;</span>
         <span>{{ directionFloor }}</span>
